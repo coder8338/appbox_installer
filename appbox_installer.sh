803b9ddd0fd2bc79db39ba5e05e3457034e14afe
coder8338 <64418056+coder8338@users.noreply.github.com>
2021-04-02 10:44:24 +0100

    Fixes

--- appbox_installer.sh
+++ appbox_installer.sh
@@ -131,7 +131,7 @@ fdmove -c 2 1
 s6-setuidgid appbox
 
 foreground { rm /home/appbox/.config/Sonarr/sonarr.pid }
-/usr/bin/mono --debug /usr/lib/sonarr/bin/Sonarr.exe -nobrowser"
+/usr/bin/mono --debug /usr/lib/sonarr/bin/Sonarr.exe -nobrowser
 EOF
 )
     create_service 'sonarr'
@@ -272,6 +272,7 @@ setup_sabnzbdplus() {
     add-apt-repository -y ppa:jcfp/ppa
     apt-get install -y sabnzbdplus
     sed -i 's/--daemon//g' /etc/init.d/sabnzbdplus
+    mkdir /home/appbox/.sabnzbd
     cat << EOF > /etc/default/sabnzbdplus
 USER=appbox
 HOST=0.0.0.0
@@ -286,6 +287,7 @@ fdmove -c 2 1
 
 s6-setuidgid appbox
 
+cd /home/appbox/.sabnzbd
 /usr/bin/python3 -OO /usr/bin/sabnzbdplus --pidfile /var/run/sabnzbdplus/pid --server 0.0.0.0:9090 -b0
 EOF
 )
